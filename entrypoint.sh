1304ae9a8996af6208ced0150ed9acf601d9ba09
Esteban Chacon Martin <esteban@godjango.dev>
2025-05-07 11:33:16 -0400

    fix: Passing username from the entrypoint for automatically create the superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 python manage.py migrate
-python manage.py createsuperuser --no-input
-gunicorn --access-logfile /var/log/app/logfile $(ls */wsgi.py | cut -d / -f 1).wsgi:application --bind :8000
+# TODO Please use a secret file path with something like $(cat mysecret) for this, perhaps or something securer
+python manage.py createsuperuser --no-input --username=$USERNAME
+gunicorn $(ls */wsgi.py | cut -d / -f 1).wsgi:application --bind :8000
