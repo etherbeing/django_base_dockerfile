e4657a202766f5ef49ad4fb73d778ca6f415c553
Esteban Chacon Martin <esteban@godjango.dev>
2025-05-07 11:02:20 -0400

    fix: Using alternative sh path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 python manage.py migrate
 python manage.py createsuperuser --no-input
 gunicorn --access-logfile /var/log/app/logfile $(ls */wsgi.py | cut -d / -f 1).wsgi:application --bind :8000
