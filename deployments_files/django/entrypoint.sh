4f078083a8dd627f290077949f1be6c967e09aa7
Esteban Chacon Martin <esteban@godjango.dev>
2025-05-14 13:43:32 -0400

    feat: Add support for daphne based deployments

--- deployments_files/django/entrypoint.sh
+++ deployments_files/django/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+MAIN_APP=$(find . -name wsgi.py | head -n1 | cut -d/ -f2)
+
 if [ ! -f "$STATICFILES_PATH" ]; then
   yes yes | python manage.py collectstatic
 fi
 python manage.py migrate
 # TODO Please use a secret file path with something like $(cat mysecret) for this, perhaps or something securer
 eval $(cat .env | grep USERNAME) python manage.py createsuperuser --no-input --username=$USERNAME --email=$EMAIL
-gunicorn $(ls */wsgi.py | cut -d / -f 1).wsgi:application --bind :8000
+if [ -z "$USE_DAPHNE" ] || [ "$USE_DAPHNE" = "False"]; then
+  gunicorn $MAIN_APP.wsgi:application --bind :8000
+else
+  daphne $MAIN_APP.asgi:application --bind :8000
+fi
